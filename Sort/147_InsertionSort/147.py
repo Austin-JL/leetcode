f2fd88a6df53f7ce0fcbda33a51ca30b8b91f28f
Austin <j543zhan@uwaterloo.ca>
2020-06-20 17:41:42 -0400

    code

---
+++ Sort/147_InsertionSort/147.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        ans = ListNode(0)
+        ans.next = head
+        
+        while head and head.next:
+            if head.val <= head.next.val:
+                head = head.next
+            else:
+                tmp = ans
+                node = head.next
+                
+                while tmp.next.val < node.val:
+                    tmp = tmp.next
+                # 
+                head.next = node.next
+                #
+                tmp.next, node.next = node,tmp.next
+                
+        return ans.next
